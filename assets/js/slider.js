036b6751dd649cdae32e8f58e33de16debdcd118
Anthony Rafael <anthonyribeiro1910@gmail.com>
2021-10-29 23:59:57 -0300

    Adding link to place

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -246,7 +246,7 @@ function createBlock(galleryElement, location) {
 
     card.querySelector("h2").addEventListener("click", () => {
 
-        window.open(location.link, "_blank");
+        window.open(location.site, "_blank");
 
     });
 
